d8ac23598df2bfd39c371bcf0778148a28f24db2
danielgf50 <danielgf50@educastur.es>
2024-12-05 17:03:38 +0100

    cambio a contador

--- src/ejercicios/depuracion/Ejemplo1While.java
+++ src/ejercicios/depuracion/Ejemplo1While.java
@@ -14,5 +14,10 @@ public class Ejemplo1While {
         }
         System.out.println("La suma es: " + suma );
     }
+    private int suma, contador;
+    public Ejemplo1While() {
+    this.suma = 0;
+    this.contador = 0;
+    }
 }
 
